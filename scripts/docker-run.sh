5a453de10403af681f66c434b8716ee18b9dcfc1
wsgggws <wsgggws@gmail.com>
2025-06-30 23:59:35 +0800

    refactor: remove nginx

--- scripts/docker-run.sh
+++ scripts/docker-run.sh
@@ -13,16 +13,5 @@ set +a
 echo "🚀 启动容器 web celery-worker celery-beat."
 docker compose up --build web celery-worker celery-beat -d
 
-# nginx: 启动或重启
-NGINX_ID=$(docker compose ps -q nginx)
-
-if [[ -z "$NGINX_ID" ]] || [[ "$(docker inspect -f '{{.State.Running}}' "$NGINX_ID" 2>/dev/null)" != "true" ]]; then
-  echo "🚀 nginx 未启动，开始启动 nginx"
-  docker compose up -d nginx
-else
-  echo "🔁 nginx 已在运行，执行重启"
-  docker compose restart nginx
-fi
-
 echo "🧼 删除旧 <none> 镜像..."
 docker images --filter "dangling=true" -q | xargs -r docker rmi
