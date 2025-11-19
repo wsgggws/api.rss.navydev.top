c6141284295c2b14dc63611ae65806dbd8c72948
wsgggws <wsgggws@gmail.com>
2025-11-19 23:19:29 +0800

    refactor: tests due upgrade pytest-asyncio

--- tests/user/conftest.py
+++ tests/user/conftest.py
@@ -5,7 +5,7 @@ from tests.helper import the_first_user, the_second_user
 
 
 # module 意味着在 user 这个模块下执行一次
-@pytest_asyncio.fixture(scope="module", autouse=True)
+@pytest_asyncio.fixture(scope="function", autouse=True)
 async def setup_user_in_database(client: AsyncClient):
     # 插入两个个测试用户
     response = await client.post("/api/v1/user/register", json=the_first_user)
